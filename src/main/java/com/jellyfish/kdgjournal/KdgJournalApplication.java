06c41a20225265c609b4f56d8d7ebf40cd6f573e
顾显雄 <guxianxiong@insaic.com>
2020-05-12 18:57:51 +0800

    first commit

---
+++ src/main/java/com/jellyfish/kdgjournal/KdgJournalApplication.java
@@ -0,0 +1,13 @@
+package com.jellyfish.kdgjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdgJournalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KdgJournalApplication.class, args);
+  }
+
+}
